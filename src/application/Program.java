14a4ea4c9d40d0688bbd5390a6d5a234f84aaed7
Pedro Rhamon <pedrorhamon16@gmail.com>
2021-01-28 20:47:46 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,10 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("=== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Matha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update complete!");		
 	}
 }
